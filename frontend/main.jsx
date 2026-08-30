3f012972fc26c0167df1d56386a5b14c569111a0
Jimmi <gezimtahiri26@gmail.com>
2026-08-30 20:44:56 +0200

    Add files via upload

--- frontend/main.jsx
+++ frontend/main.jsx
@@ -4,7 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import {I18nProvider} from './i18n/I18n'
 import './styles/global.css'
-import './styles/desktop-login-exact.css'
+import './styles/desktop-tablet-final.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
