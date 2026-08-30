8c57765299173d14c3d01936ab8c7183c701b218
Jimmi <gezimtahiri26@gmail.com>
2026-08-30 19:34:30 +0200

    Add files via upload

---
+++ frontend/main.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import { HashRouter } from 'react-router-dom'
+import App from './App'
+import {I18nProvider} from './i18n/I18n'
+import './styles/global.css'
+import './styles/desktop-tablet.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <HashRouter>
+      <I18nProvider><App /></I18nProvider>
+    </HashRouter>
+  </React.StrictMode>
+)
